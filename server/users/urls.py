717b430a36898b43bc8c2534fcdc74b88bf7783c
kothawleprem <kothawleprem@gmail.com>
2023-08-28 11:31:10 +0530

    updated project structure

--- server/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import EmailView, VerifyEmailView
-
-urlpatterns = [
-    path('email/', EmailView.as_view(), name="email"),
-    path('verify_email/', VerifyEmailView.as_view(), name="verify_email"),
-
-]
